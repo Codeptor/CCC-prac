2c090edd384480dc085702f282ef699a2c81d81a
Bhanu <bhanu9112002@gmail.com>
2023-03-10 15:58:10 +0530

    poggers

---
+++ p12.py
@@ -0,0 +1,5 @@
+#write a program to print sum of first and last digit of a given number.
+num=(int)(input("Enter a number: "))
+first_digit=int(num[0])
+last_digit=int(num[-1])
+print(first_digit+last_digit)
